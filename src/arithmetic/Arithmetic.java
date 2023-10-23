276f5f50b13f403f2fba4dc490644a7d51457fbb
Ashwinjojo <145361173+Ashwinjojo@users.noreply.github.com>
2023-10-23 19:55:32 +0500

    Update Arithmetic.java

--- src/arithmetic/Arithmetic.java
+++ src/arithmetic/Arithmetic.java
@@ -5,32 +5,18 @@
  */
 package arithmetic;
 
+public class Arithmetic {
+    public static void main(String[] args) {
+        ArithmeticBase r = new ArithmeticBase();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        double x = sc.nextDouble();
+        System.out.println("Enter the second number: ");
+        double y = sc.nextDouble();
 
-import java.util.Scanner;
-import static java.time.Clock.system;
-
-/** This class calls the method to perform 
- * arithmetic operations based on user input
- * execute the code check the output
- * @author sivagamasrinivasan
- * 
- */
-public class Arithmetic 
-{
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-       
-        ArithmeticBase r= new ArithmeticBase();
-        Scanner in= new Scanner(System.in);
-        int n= in.nextInt();
-        int m= in.nextInt();
-        double result = r.calculate(m,n);
-        System.out.println("result :" +result); 
-    
+        for (ArithmeticOperation operation : ArithmeticOperation.values()) {
+            double result = r.calculate(x, y, operation);
+            System.out.println(operation + " result: " + result);
+        }
     }
 }
-
